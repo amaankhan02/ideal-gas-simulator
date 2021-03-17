7e10acd136de438eaa7863df3e9a96f8a7bd935f
Amaan Khan <amaanite@gmail.com>
2021-03-16 23:54:49 -0500

    Wrote more tests for SimpleParticlesPhysicsEngine

--- tests/test_simple_particles_physics_engine.cc
+++ tests/test_simple_particles_physics_engine.cc
@@ -60,8 +60,33 @@ TEST_CASE("UpdateParticleVelocity for particle collisions") {
   }
 
   SECTION("When particles are touching but already moving away") {
+    Particle particle1(vec2(50, 50), vec2(2, -2), ci::Color("red"), 10);
+    Particle particle2(vec2(55, 56), vec2(3, 3), ci::Color("red"), 10);
+
+    physicsEngine.UpdateParticleVelocity(
+        particle1, particle2, container_top_left, container_bottom_right);
+
+    REQUIRE(particle1.GetVelocity() == vec2(2.1, -1.8));
+    REQUIRE(particle2.GetVelocity() == vec2(4.8, -5.2));
   }
 }
 
 TEST_CASE("Test FindNearestParticleIndex") {
+  SimpleParticlePhysicsEngine physicsEngine = SimpleParticlePhysicsEngine();
+  Particle particle1(vec2(10, 9), vec2(1, -1), ci::Color("red"), 10);
+  Particle particle2(vec2(20, 20), vec2(1, -1), ci::Color("red"), 10);
+  Particle particle3(vec2(90, 90), vec2(1, -1), ci::Color("red"), 10);
+  Particle particle4(vec2(70, 70), vec2(1, -1), ci::Color("red"), 10);
+  vector<Particle> particles = {particle1, particle2, particle3, particle4};
+  vector<Particle> single_vector_particles = {particle1};
+
+  SECTION("Testing 4 particles") {
+    int index = physicsEngine.FindNearestParticleIndex(0, particles);
+    REQUIRE(index == 1);
+  }
+
+  SECTION("Testing 1 particles list") {
+    int index = physicsEngine.FindNearestParticleIndex(0, single_vector_particles);
+    REQUIRE(index == 0);
+  }
 }
