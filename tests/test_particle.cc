a763fcc50398d561531e9a9eca12fc75045560a1
Amaan Khan <amaanite@gmail.com>
2021-03-23 20:00:08 -0500

    Made modifications for removing physics engine class

--- tests/test_particle.cc
+++ tests/test_particle.cc
@@ -2,51 +2,74 @@
 
 #include <catch2/catch.hpp>
 
-#include "simple_particle_physics_engine.h"
-
 using idealgas::Particle;
 
-TEST_CASE("Test Constructor: Important values initialized correctly") {
+TEST_CASE("Test Particle Constructor: Important values initialized correctly") {
   vec2 position(2, 3);
   vec2 velocity(4, 5);
-  Particle particle(position, velocity, ci::Color("red"), 10);
+  Particle particle(position, velocity, ci::Color("red"), 10, 15, "MyName");
 
   REQUIRE(particle.GetColor() == "red");
   REQUIRE(particle.GetRadius() == 10);
   REQUIRE(particle.GetPosition() == position);
   REQUIRE(particle.GetVelocity() == velocity);
+  REQUIRE(particle.GetMass() == 15);
+  REQUIRE(particle.GetTypeName() == "MyName");
 }
 
-TEST_CASE("Move function") {
+TEST_CASE("Move Function") {
   vec2 position(2, 3);
   vec2 velocity(4, 5);
-  Particle particle(position, velocity, ci::Color("red"), 10);
+  Particle particle(position, velocity, ci::Color("red"), 10, 15, "MyName");
 
   SECTION("Move with default velocity") {
     particle.Move();
     REQUIRE(particle.GetPosition() == vec2(6, 8));
   }
 
-  SECTION(
-      "Move after changing velocity - does it move to the updated velocity?") {
-    particle.SetVelocityX(-2);
-    particle.SetVelocityY(-5);
-    REQUIRE(particle.GetPosition() == vec2(0, -2));
+  SECTION("Does it move after updating velocity") {
+    particle.UpdateVelocityForHorizontalWallCollision();
+    particle.Move();
+    REQUIRE(particle.GetPosition() == vec2(6, -2));
   }
 }
 
-TEST_CASE("Set Velocity Function") {
-  vec2 position(2, 3);
-  vec2 velocity(4, 5);
-  Particle particle(position, velocity, ci::Color("red"), 10);
+TEST_CASE("Wall Collisions") {
+  float mass = 15;
+  std::string name = "MyName";
+  Particle particle1(vec2(10, 9), vec2(1, -1), ci::Color("red"), 10, mass, name);
+  Particle particle2(vec2(20, 20), vec2(1, -1), ci::Color("red"), 10, mass, name);
 
-  SECTION("Set x component") {
-    particle.SetVelocityX(7);
-    REQUIRE(particle.GetPosition() == vec2(7, 5));
+  SECTION("Collide with vertical wall") {
+    particle1.UpdateVelocityForVerticalWallCollision();
+    REQUIRE(particle1.GetVelocity()[0] == -1.0f);
   }
 
-  SECTION("Set y component") {
-    particle.SetVelocityY(7);
-    REQUIRE(particle.GetPosition() == vec2(4, 7));
+  SECTION("Collide with top wall") {
+    particle2.UpdateVelocityForHorizontalWallCollision();
+    REQUIRE(particle1.GetVelocity()[1] == -1.0f);
+  }
+}
+
+TEST_CASE("Particle to Particle Collisions") {
+  vec2 container_top_left(0, 0);
+  vec2 container_bottom_right(100, 100);
+
+  SECTION("When particles are touching & approaching each other") {
+    Particle particle1(vec2(50, 50), vec2(2, -2), ci::Color("red"), 10, 15, "RedParticle");
+    Particle particle2(vec2(55, 56), vec2(-3, 3), ci::Color("blue"), 10, 20, "BlueParticle");
+
+    particle1.UpdateVelocitiesForParticleCollision(particle2);
+    REQUIRE(particle1.GetVelocity() == vec2(1.9, -2.1));
+    REQUIRE(particle2.GetVelocity() == vec2(5, -5.4));
+  }
+
+  SECTION("When particles are touching but already moving away") {
+    Particle particle1(vec2(50, 50), vec2(2, -2), ci::Color("red"), 10, 15, "RedParticle");
+    Particle particle2(vec2(48, 48), vec2(-3, 3), ci::Color("blue"), 10, 20, "BlueParticle");
+
+    particle1.UpdateVelocitiesForParticleCollision(particle2);
+    REQUIRE(particle1.GetVelocity() == vec2(2, -2));
+    REQUIRE(particle2.GetVelocity() == vec2(-3, 3));
   }
 }
