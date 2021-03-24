0756175b746769c83700a74867ea0dfc9819db2d
Amaan Khan <amaanite@gmail.com>
2021-03-23 21:50:21 -0500

    Added new edge case tests

--- tests/test_gas_container.cc
+++ tests/test_gas_container.cc
@@ -22,10 +22,10 @@ TEST_CASE("Test Constructor: Important values initialized correctly") {
 }
 
 TEST_CASE("AdvanceOneFrame Method") {
-  // particle1 & 2 are colliding. Particle 3 collide w/ wall. Particle 4 is free
-  Particle particle1(vec2(110, 110), vec2(3, -3), ci::Color("red"), 10, 10,
+  // particle1 & 2 are colliding. Particle 3 collide w/ wall. Particle 4 is far away
+  Particle particle1(vec2(111, 111), vec2(3, -3), ci::Color("red"), 10, 10,
                      "RED");
-  Particle particle2(vec2(111, 111), vec2(-3, 3), ci::Color("blue"), 10, 15,
+  Particle particle2(vec2(110, 110), vec2(-3, 3), ci::Color("blue"), 10, 15,
                      "BLUE");
   Particle particle3(vec2(395, 200), vec2(2, -2), ci::Color("red"), 10, 10,
                      "RED");
@@ -33,7 +33,7 @@ TEST_CASE("AdvanceOneFrame Method") {
                      "BLUE");
   vector<Particle> particles = {particle1, particle2, particle3, particle4};
   GasContainer container(3, vec2(100, 100), vec2(300, 300), 5, particles);
-
+  container.AdvanceOneFrame();
   SECTION(
       "Check that particles update velocities and move to correct position") {
     REQUIRE(particle1.GetPosition() == vec2(108.2f, 113.5f));
@@ -48,5 +48,18 @@ TEST_CASE("AdvanceOneFrame Method") {
     REQUIRE(speeds.at("BLUE")[0] == 3.68f);
     REQUIRE(speeds.at("RED")[1] == 2.83f);
     REQUIRE(speeds.at("BLUE")[1] == 1);
+  }
+
+  SECTION("Check if particle touching but not approaching each other it doesn't call to change velocity") {
+    Particle particle5(vec2(110, 110), vec2(3, -3), ci::Color("red"), 10, 10,
+                       "RED");
+    Particle particle6(vec2(115, 115), vec2(-6, 6), ci::Color("blue"), 10, 15,
+                       "BLUE");
+    vector<Particle> particles2 = {particle5, particle6};
+    GasContainer container2(3, vec2(100, 100), vec2(300, 300), 5, particles);
+    container.AdvanceOneFrame();
+
+    REQUIRE(particle5.GetPosition() == vec2(110, 110));
+    REQUIRE(particle6.GetPosition() == vec2(115, 115));
   }
 }
