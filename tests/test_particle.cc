aacc1042c1857003cbd45235c3ed905032d9dceb
Amaan Khan <amaanite@gmail.com>
2021-03-23 21:34:48 -0500

    Added new edge case tests for collision

--- tests/test_particle.cc
+++ tests/test_particle.cc
@@ -2,8 +2,8 @@
 
 #include <catch2/catch.hpp>
 
-using idealgas::Particle;
 using glm::vec2;
+using idealgas::Particle;
 
 TEST_CASE("Test Particle Constructor: Important values initialized correctly") {
   vec2 position(2, 3);
@@ -38,17 +38,29 @@ TEST_CASE("Move Function") {
 TEST_CASE("Wall Collisions") {
   float mass = 15;
   std::string name = "MyName";
-  Particle particle1(vec2(10, 9), vec2(1, -1), ci::Color("red"), 10, mass, name);
-  Particle particle2(vec2(20, 20), vec2(1, -1), ci::Color("red"), 10, mass, name);
+  Particle particle1(vec2(10, 9), vec2(1, -1), ci::Color("red"), 10, mass,
+                     name);
+  Particle particle2(vec2(20, 20), vec2(1, -1), ci::Color("red"), 10, mass,
+                     name);
 
-  SECTION("Collide with vertical wall") {
+  SECTION("Collide with top wall") {
     particle1.UpdateVelocityForVerticalWallCollision();
     REQUIRE(particle1.GetVelocity()[0] == -1.0f);
   }
 
-  SECTION("Collide with top wall") {
+  SECTION("Collide with bottom wall") {
+    particle1.UpdateVelocityForVerticalWallCollision();
+    REQUIRE(particle1.GetVelocity()[0] == 1.0f);
+  }
+
+  SECTION("Collide with right wall") {
     particle2.UpdateVelocityForHorizontalWallCollision();
     REQUIRE(particle1.GetVelocity()[1] == -1.0f);
+  }
+
+  SECTION("Collide with left wall") {
+    particle2.UpdateVelocityForHorizontalWallCollision();
+    REQUIRE(particle1.GetVelocity()[1] == 1.0f);
   }
 }
 
@@ -57,8 +69,10 @@ TEST_CASE("Particle to Particle Collisions") {
   vec2 container_bottom_right(100, 100);
 
   SECTION("When particles are touching & approaching each other") {
-    Particle particle1(vec2(50, 50), vec2(2, -2), ci::Color("red"), 10, 15, "RedParticle");
-    Particle particle2(vec2(55, 56), vec2(-3, 3), ci::Color("blue"), 10, 20, "BlueParticle");
+    Particle particle1(vec2(50, 50), vec2(2, -2), ci::Color("red"), 10, 15,
+                       "RedParticle");
+    Particle particle2(vec2(55, 56), vec2(-3, 3), ci::Color("blue"), 10, 20,
+                       "BlueParticle");
 
     particle1.UpdateVelocitiesForParticleCollision(particle2);
     REQUIRE(particle1.GetVelocity() == vec2(1.9, -2.1));
@@ -66,11 +80,43 @@ TEST_CASE("Particle to Particle Collisions") {
   }
 
   SECTION("When particles are touching but already moving away") {
-    Particle particle1(vec2(50, 50), vec2(2, -2), ci::Color("red"), 10, 15, "RedParticle");
-    Particle particle2(vec2(48, 48), vec2(-3, 3), ci::Color("blue"), 10, 20, "BlueParticle");
+    Particle particle1(vec2(50, 50), vec2(2, -2), ci::Color("red"), 10, 15,
+                       "RedParticle");
+    Particle particle2(vec2(48, 48), vec2(-3, 3), ci::Color("blue"), 10, 20,
+                       "BlueParticle");
 
     particle1.UpdateVelocitiesForParticleCollision(particle2);
     REQUIRE(particle1.GetVelocity() == vec2(2, -2));
     REQUIRE(particle2.GetVelocity() == vec2(-3, 3));
+  }
+
+  SECTION("2 particles approaching & touching, and with 3rd far away") {
+    Particle particle1(vec2(50, 50), vec2(2, -2), ci::Color("red"), 10, 15,
+                       "RedParticle");
+    Particle particle2(vec2(48, 48), vec2(-3, 3), ci::Color("blue"), 10, 20,
+                       "BlueParticle");
+    Particle particle3(vec2(100, 100), vec2(-3, 3), ci::Color("blue"), 10, 20,
+                       "CoolParticle");
+
+    particle1.UpdateVelocitiesForParticleCollision(particle2);
+    REQUIRE(particle1.GetVelocity() == vec2(1.82, -2.4));
+    REQUIRE(particle2.GetVelocity() == vec2(-3.4, 3.98));
+    REQUIRE(particle3.GetVelocity() == vec2(-3, 3));
+  }
+
+  SECTION(
+      "More than 2 particles, with 3rd touching another but not approaching "
+      "either") {
+    Particle particle1(vec2(50, 50), vec2(-1, 0), ci::Color("red"), 10, 15,
+                       "RedParticle");
+    Particle particle2(vec2(34, 50), vec2(1, 0), ci::Color("blue"), 10, 20,
+                       "BlueParticle");
+    Particle particle3(vec2(42, 50), vec2(0, 1), ci::Color("blue"), 10, 20,
+                       "CoolParticle");
+
+    particle1.UpdateVelocitiesForParticleCollision(particle2);
+    REQUIRE(particle1.GetVelocity() == vec2(2, -2));
+    REQUIRE(particle2.GetVelocity() == vec2(-3, 3));
+    REQUIRE(particle3.GetVelocity() == vec2(0, 1));
   }
 }
